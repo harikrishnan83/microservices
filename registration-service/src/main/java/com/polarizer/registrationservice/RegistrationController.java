a521be6b9261eb793478804de9f688368a102c3a
Hari Krishnan <harikrishnan83@gmail.com>
2017-07-03 19:48:05 +0530

    Adding FeignClient

--- registration-service/src/main/java/com/polarizer/registrationservice/RegistrationController.java
+++ registration-service/src/main/java/com/polarizer/registrationservice/RegistrationController.java
@@ -3,22 +3,25 @@ package com.polarizer.registrationservice;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableDiscoveryClient
 @RestController
 public class RegistrationController {
 	
 	@Autowired
-	private DiscoveryClient discoveryClient;
+	private UserServiceClient userServiceClient;
 	
 	@RequestMapping("/")
-    public String index() {
-		List<ServiceInstance> instances = this.discoveryClient.getInstances("user-service");
-        return "Registration Service " + instances.get(0);
+	public String hello() {
+		return "Registration Service";
+	}
+	
+	@RequestMapping("/registrations")
+    public List<User> index() {
+//		List<ServiceInstance> instances = this.discoveryClient.getInstances("user-service");
+//		UserServiceClient userServiceClient = Feign.builder().target(UserServiceClient.class, "http://user-service");
+		System.out.println("-------" + userServiceClient.toString());
+        return userServiceClient.findAll();
     }
 }
--- user-service/src/main/java/com/polarizer/user/UserController.java
+++ user-service/src/main/java/com/polarizer/user/UserController.java
@@ -1,12 +1,23 @@
 package com.polarizer.user;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class UserController {
 	@RequestMapping("/")
-    public String index() {
-        return "User Service";
-    }
+	public String hello() {
+		return "User Service";
+	}
+	
+	@RequestMapping("/users")
+    public List<User> index() {
+		List<User> users = new ArrayList<User>();
+		users.add(new User("hari@polarizer.in", "Hari Krishnan"));
+        return users;
+    }	
 }
