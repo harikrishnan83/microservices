c620a72fa7203367c9a39c3d2a8b7e1b2cd7aacb
Hari Krishnan <harikrishnan83@gmail.com>
2017-07-04 15:01:11 +0530

    adding user-service load balancing

--- registration-service/src/main/java/com/polarizer/registrationservice/RegistrationServiceApplication.java
+++ registration-service/src/main/java/com/polarizer/registrationservice/RegistrationServiceApplication.java
@@ -1,9 +1,14 @@
 package com.polarizer.registrationservice;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,5 +17,11 @@ public class RegistrationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationServiceApplication.class, args);
+	}
+	
+	@FeignClient("polarizer-user-service")
+	interface UserServiceClient {
+		@RequestMapping(method = RequestMethod.GET, value = "/users")
+	    List<User> findAll();
 	}
 }
