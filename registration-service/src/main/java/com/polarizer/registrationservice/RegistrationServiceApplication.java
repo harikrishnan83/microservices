bd8192dea6a90d07ce0531ffdfcd210e87b6cf5a
Hari Krishnan <harikrishnan83@gmail.com>
2017-07-05 07:41:47 +0530

    adding Hystrix fallback for user-service

--- registration-service/src/main/java/com/polarizer/registrationservice/RegistrationServiceApplication.java
+++ registration-service/src/main/java/com/polarizer/registrationservice/RegistrationServiceApplication.java
@@ -1,27 +1,20 @@
 package com.polarizer.registrationservice;
 
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
+@EnableHystrix
 public class RegistrationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationServiceApplication.class, args);
-	}
-	
-	@FeignClient("polarizer-user-service")
-	interface UserServiceClient {
-		@RequestMapping(method = RequestMethod.GET, value = "/users")
-	    List<User> findAll();
 	}
 }
