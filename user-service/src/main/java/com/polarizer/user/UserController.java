379c1a04d864a0e7393d53a88ec424725f4fc891
Hari Krishnan <harikrishnan83@gmail.com>
2017-07-12 00:31:45 +0530

    getting principal in user-service

--- user-service/src/main/java/com/polarizer/user/UserController.java
+++ user-service/src/main/java/com/polarizer/user/UserController.java
@@ -2,6 +2,7 @@ package com.polarizer.user;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,16 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class UserController {
 	@RequestMapping("/")
-	public String hello() {
+	public String hello(Principal principal) {
+		System.out.println("Requested by - " + principal.getName());
 		return "User Service";
 	}
 	
 	@RequestMapping("/users")
-    public List<User> index() {
+    public List<User> index(Principal principal) {
+		System.out.println("Requested by - " + principal.getName());
 		List<User> users = new ArrayList<User>();
 		users.add(new User("hari@polarizer.in", "Hari Krishnan"));
 		users.add(new User("nalini@polarizer.in", "Nalini"));
-		System.out.println("place holder app log");
-        return users;
+		return users;
     }	
 }
